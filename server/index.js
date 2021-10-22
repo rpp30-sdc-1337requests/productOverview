2460431403d36c1e1ca5399606e39b475bdf4682
Ashley Reischman <ashleyreischman648@gmail.com>
2021-10-21 20:06:54 -0700

    took out connecting and running the functions to import from index. all handled in the schemas file now

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
+const express = require ('express');
+const app = express();
+const port = 3000;
+const mongoose = require ('mongoose');
 
-const testDummyFunction = () => {
-  return ('hello');
-}
 
-module.exports = { testDummyFunction }
+const databaseHelpers = require('./database/schemas.js');
+
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+})
+
+
+
+
