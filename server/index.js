ca8432e0a515e5dcad68b97fcce345d07aa4abf1
Ashley Reischman <ashleyreischman648@gmail.com>
2021-10-31 11:41:37 -0700

    added routes for styles/related products/products. all product overview requests are working with the client

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 const express = require ('express');
 const app = express();
-const port = 3000;
+const port = 3009;
 const mongoose = require ('mongoose');
 const dbURL = 'mongodb://localhost:27017/products';
 const productModel = require('./database/models/products.js').Product
 const featureModel = require('./database/models/features.js').Feature
 const relatedModel = require('./database/models/related.js').Related
+const stylesAggedByProdModel = require('./database/models/styles.js').StylesAggedByProd
+
 //for reimport only
 // const databaseHelpers = require('./database/schemas.js');
-//for reimport only
+
 // app.listen(port, () => {
 //   console.log(`Server is up on port ${port}`);
 // })
@@ -24,24 +26,54 @@ mongoose.connect(dbURL)
 })
 
 
+//universal get
+// app.get('/*', (req, res) => {
+//   console.log('Received something from someone!!!');
+// })
+//style
+//
+app.get('*/styles', async (req, res) => {
+  console.log('check for params');
+  try {
+    let result = await stylesAggedByProdModel.find({product_id: parseInt(req.params[0].slice(10,req.params[0].length))}).lean();
+    if (result.length === 0) {
+      res.status(200).send(result);
+    } else {
+      let skusObj = {};
+      for (let i = 0; i < result[0].results.length; i++) {
+        for (let j = 0; j < result[0].results[i].skus.length; j++) {
+          skusObj[result[0].results[i].skus[j].sku_id.toString()] = result[0].results[i].skus[j]
+        }
+        result[0].results[i].skus = skusObj;
+        skusObj = {}
+      }
+      res.status(200).send(result[0]);
+      console.log(result);
+    }
+  } catch (err) {
+    res.status(500).send(err)
+  }
+
+})
+
 //products/product_id
 //TO-DO - figure out how to separate these.
 //also figure out how to use index instead of find
-app.get('/products', async (req, res) => {
+app.get('/products/*', async (req, res) => {
   console.log(req.params);
-  if (req.query.product_id === undefined) {
+  let productId = parseInt(req.params[0]);
+  if (productId === undefined) {
     console.log('inside products route');
     //stand in for the framework i will eventually get to that includes listening to parameters.
     let result = await productModel.find({product_id: 1}).lean();
     res.status(200).send(result[0]);
   } else {
     Promise.all([
-      productModel.find({product_id: req.query.product_id}).lean(),
-      featureModel.find({product_id: req.query.product_id}).lean()
+      productModel.find({product_id: productId}).lean(),
+      featureModel.find({product_id: productId}).lean()
     ])
     .then ((data) => {
-      let result = {
-      };
+      let result = {};
       result['product_id'] = data[0][0].product_id;
       result['name'] = data[0][0].name;
       result['slogan'] = data[0][0].slogan;
@@ -67,17 +99,16 @@ app.get('/products', async (req, res) => {
 //   res.status(200).send(result[0]);
 // });
 
-//style
-//
-app.get('*/styles', (req, res) => {
-///i need to aggregate the data together for this route, otherwise i'm gonna need like 19 different queries per request.....
-})
-
 //related
 //also needs indexing.
 app.get('*/related', async (req, res) => {
-  let result = await relatedModel.find({productId: req.query.product_id}).lean();
-  res.status(200).send(result[0].relatedProducts);
+  console.log('related request recieved');
+  try {
+    let result = await relatedModel.find({productId: req.query.product_id}).lean();
+    res.status(200).send(result[0].relatedProducts);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 })
 
 
