5f105717b8c7a56dc96f172c20ef1f37c6f3487f
Ashley Reischman <ashleyreischman648@gmail.com>
2021-11-14 19:46:17 -0800

    added new relic into index.js, updated package.json to include new relic module, updated k6Test

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const app = express();
 const port = 80;
+require('newrelic');
 var server;
 const mongoose = require ('mongoose');
 const DB_USER = require('./config.js').DB_USER;
